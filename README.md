278883d85e0c81d271f3f8db95660bc5fe0d19fe
Fantix King <fantix@uchicago.edu>
2020-01-03 11:22:33 -0600

    a working example to serve /dcms on localhost:8080

---
+++ README.md
@@ -0,0 +1,8 @@
+Put [DICOM](https://en.wikipedia.org/wiki/DICOM)(.dcm) files under `dcms/` and run:
+
+```shell
+docker-compose up
+```
+
+Visit http://localhost:8080/ to see them.
+
---
+++ auto-index.lua
@@ -0,0 +1,21 @@
+function Initialize()
+    local pfile = assert(io.popen("find '/data/' -mindepth 1 -iname '*.dcm' -type f -printf '/data/%f\\0'", 'r'))
+    local list = pfile:read('*a')
+    pfile:close()
+
+    for filename in string.gmatch(list, '[^%z]+') do
+        local f = assert(io.open(filename, 'rb'))
+        local resp = ParseJson(RestApiPost('/instances', f:read('*all')))
+        f:close()
+        if resp['Status'] == 'Success' then
+            local uuid = ParseJson(RestApiGet(resp['Path']))['FileUuid']
+            local path = '/var/lib/orthanc/db/' .. uuid:sub(1, 2) .. '/' .. uuid:sub(3, 4) .. '/'
+            local p = assert(io.popen('mkdir -p ' .. path))
+            p:read('*a')
+            p:close()
+            local p = assert(io.popen('ln -sf ' .. filename .. ' ' .. path .. uuid))
+            p:read('*a')
+            p:close()
+        end
+    end
+end
