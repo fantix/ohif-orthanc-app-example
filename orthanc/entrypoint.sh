7e8cce13b54305880b0b4f626f94f5397e0df260
Fantix King <fantix@uchicago.edu>
2020-01-07 11:45:59 -0600

    wait for fuse

--- orthanc/entrypoint.sh
+++ orthanc/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-while ! psql -h localhost -U postgres orthanc -c 'select now()'; do sleep 0.1; done
+while ! psql -h localhost -U postgres orthanc -c 'select now()'; do sleep 0.5; done
+while ! mount | grep '/data/'; do sleep 0.5; done
 Orthanc "$@"
